1ac5219cdc96293a2b9540fbc714dcc02d432fd6
Sasha Berkowsky <s.nberkowsky@gmail.com>
2021-07-13 20:09:31 -0300

    cambios en choice

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="jumbotron p-3">
     <Codificado />
     <hr />
-    <p>Respuestas: 1-B, 2-B, 3-C, 4-B, 5-C</p>
+    <p>Respuestas: 1-B, 2-B, 3-C, 4-B, 5-B y C</p>
   </div>
 </template>
 
